49cc3a0c0d871aa881af901520bf2cb6ff74a602
Marcio Dev <devmarciofaria@gmail.com>
2022-03-03 04:04:55 -0400

    token

--- src/service/api.js
+++ src/service/api.js
@@ -1,10 +1,8 @@
 import axios from "axios"
 
-let tokenStr = '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'
 
 const api = axios.create({
-    baseURL:"https://api.github.com/users/",
-     headers: {"Authorization" : `basic ${tokenStr}`}
+    baseURL:"https://api.github.com/users/"
     
 })
 
